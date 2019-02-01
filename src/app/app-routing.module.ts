268b498c30838de978883930761bca38c2b464bc
Yording <yording9@gmail.com>
2019-02-01 11:28:43 -0500

    cambios de la clase 65

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: DashboardComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "**", redirectTo: "" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
