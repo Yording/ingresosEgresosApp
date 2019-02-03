65defcfc40a4b63ef746133f83364f4e017775c5
Yording <yording9@gmail.com>
2019-02-03 18:11:55 -0500

    sección 9: video 98 implementando el loading en ingreso y egreso

--- src/app/entry-exit/entry-exit.component.ts
+++ src/app/entry-exit/entry-exit.component.ts
@@ -1,29 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { EntryExit } from './entry-exit.model';
 import { EntryExitService } from './entry-exit.service';
 import Swal from 'sweetalert2'
+import { AppState } from '../app.reducer';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import * as fromUiActions from '../shared/ui.actions'
 
 @Component({
   selector: 'app-entry-exit',
   templateUrl: './entry-exit.component.html',
   styles: []
 })
-export class EntryExitComponent implements OnInit {
+export class EntryExitComponent implements OnInit, OnDestroy {
 
   public form: FormGroup
   public type: "Ingreso" | "Egreso" = "Ingreso"
+  public loading: boolean = false
+  private _subLoading: Subscription = new Subscription()
 
-  constructor(private _entryExitService: EntryExitService) { }
+  constructor(
+    private _entryExitService: EntryExitService,
+    private store: Store<AppState>
+  ) { }
 
   ngOnInit() {
+
+    this._subLoading = this.store.select('ui')
+      .subscribe(
+        (ui) => {
+        this.loading = ui.isLoading
+      })
+
     this.form = new FormGroup({
       'description': new FormControl('', Validators.required),
       'amount': new FormControl(0, Validators.min(1))
     })
   }
 
+  ngOnDestroy(){
+    this._subLoading.unsubscribe()
+  }
+
   createEntryExit(){
+    this.store.dispatch(new fromUiActions.ActiveLoadingAction())
     const entryExit = new EntryExit({ ... this.form.value, type: this.type})
     this._entryExitService.createEntryExit(entryExit)
       .then(
@@ -34,6 +55,7 @@ export class EntryExitComponent implements OnInit {
             text: `${entryExit.type} creado satisfactoriamente.`,
             type: 'success'
           })
+          this.store.dispatch(new fromUiActions.DesactiveLoadingAction())
       })
       .catch(
       (err: Error) => {
@@ -42,7 +64,7 @@ export class EntryExitComponent implements OnInit {
             text: err.message,
             type: 'error'
           })
-        console.log(err)
+          this.store.dispatch(new fromUiActions.DesactiveLoadingAction())
       })
     
   }
