a745e201add4f3e39ffda0dffd93b28e467acf7b
Yording <yording9@gmail.com>
2019-02-03 17:24:16 -0500

    sección 9: video 96 pantalla para capturar ingreso y egreso

--- src/app/entry-exit/entry-exit.component.ts
+++ src/app/entry-exit/entry-exit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { EntryExit } from './entry-exit.model';
 
 @Component({
   selector: 'app-entry-exit',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntryExitComponent implements OnInit {
 
+  public form: FormGroup
+  public type: "Ingreso" | "Egreso" = "Ingreso"
+
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      'description': new FormControl('', Validators.required),
+      'amount': new FormControl(0, Validators.min(1))
+    })
+  }
+
+  createEntryExit(){
+    const entryExit = new EntryExit({ ... this.form.value, type: this.type})
   }
 
 }
