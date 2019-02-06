f8e8997e4df9e26e203c370abfa28f36dc25c01c
Yording <yording9@gmail.com>
2019-02-06 17:18:02 -0500

    sección 9: video 102 mostrar elementos de ingreso y egresos

--- src/app/entry-exit/detail/detail.component.ts
+++ src/app/entry-exit/detail/detail.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import { EntryExit } from '../entry-exit.model';
+import { map, filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styles: []
 })
-export class DetailComponent implements OnInit {
+export class DetailComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public items: EntryExit[] = []
+  private _subItems: Subscription = new Subscription()
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this._subItems = this.store.select('entryExit').pipe(
+      map(data => data.items),
+      filter(items => items.length > 0)
+    ).subscribe(
+      (items: EntryExit[]) => {
+        this.items = items
+        console.log(items)
+      }
+    )
+  }
+
+  ngOnDestroy(){
+    this._subItems.unsubscribe()
+  }
+
+  deleteItem(uid: string){
+    console.log(uid)
   }
 
 }
