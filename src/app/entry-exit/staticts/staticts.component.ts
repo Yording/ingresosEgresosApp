47e8be8672f8f78c0866788c11b2590908d3a484
Yording <ysanchez@conocimientocorporativo.com>
2019-02-13 17:43:06 -0500

    sección 9: video 105 dashboard estadística básica

--- src/app/entry-exit/staticts/staticts.component.ts
+++ src/app/entry-exit/staticts/staticts.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { Subscription } from 'rxjs';
+import { EntryExit } from '../entry-exit.model';
 
 @Component({
   selector: 'app-staticts',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatictsComponent implements OnInit {
 
-  constructor() { }
+  public entries: number = 0
+  public exits: number = 0
+  public amountEntries: number = 0
+  public amountExits: number = 0
+  private _sub: Subscription = new Subscription()
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this._sub = this.store.select('entryExit').subscribe(
+      (entryExit) => {
+        this.countEntryExit(entryExit.items)
+    })
+  }
+
+  countEntryExit(items: EntryExit[]){
+    this.entries = 0
+    this.exits = 0
+    this.amountEntries = 0
+    this.amountExits = 0
+    items.forEach(item => {
+      if(item.type == 'Ingreso'){
+        this.amountEntries++;
+        this.entries = this.entries + item.amount
+      }
+      else{
+        this.amountExits++;
+        this.exits = this.exits + item.amount
+      }
+    })
   }
 
 }
